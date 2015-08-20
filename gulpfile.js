a33bd22688dfdda603c3c35e3dc2ea145cedb79e
Steve Vitali <14stevevitali@gmail.com>
2015-08-19 22:20:04 -0400

    Add gulp process to compile and minify form-generator.jsx

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var react = require('gulp-react');
 
-gulp.task('default', function() {
-  console.log('Gulp');
+gulp.task('build', function() {
+  gulp.src('src/*.jsx')
+    .pipe(react())
+    .pipe(concat('form-generator.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(path.JS_SOURCE, ['build']);
+});
+
+gulp.task('default', ['watch']);
