03b4a9d8c9a623f0e67f53354e1ef35bbf7bd09b
Steve Vitali <14stevevitali@gmail.com>
2015-08-26 16:02:40 -0400

    Update gulpfile to generate the build for bower/global use

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var react = require('gulp-react');
+var replace = require('gulp-replace');
+var concat = require('gulp-concat');
 
-gulp.task('build', function() {
+// Build the version located at /dist/react-form-generator.min.js
+// that can be used globally or through bower
+gulp.task('build-global', function() {
   gulp.src('src/*.jsx')
     .pipe(react())
-    .pipe(concat('form-generator.min.js'))
+    .pipe(replace(
+      'module.exports = FormGenerator',
+      'window.FormGenerator = FormGenerator'
+    ))
+    .pipe(replace("var React = require('react');", ''))
+    .pipe(replace("var ReactBootstrap = require('react-bootstrap');", ''))
     .pipe(uglify())
+    .pipe(concat('form-generator.min.js'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/*.jsx', ['build']);
+  gulp.watch('src/*.jsx', ['build-global']);
 });
 
 gulp.task('default', ['watch']);
